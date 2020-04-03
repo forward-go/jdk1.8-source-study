a4d957f2e0c6b67f05da22c216949c5c677d4700
swcz <swcz.xf@gmail.com>
2020-04-03 19:35:56 +0800

    init

---
+++ test/Main.java
@@ -0,0 +1,11 @@
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put("1","1");
+    }
+}
